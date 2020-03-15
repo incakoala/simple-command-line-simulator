d335de824ab1ab82e3f1bbbd0222e5970042dbbc
incakoala <codervy@gmail.com>
2020-03-15 14:15:50 -0700

    Initial Commit

---
+++ HW2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HW2
+//
+//  Created by Vy Thai on 3/15/20.
+//  Copyright © 2020 Vy Thai. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
